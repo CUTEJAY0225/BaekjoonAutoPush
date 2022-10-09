1a33aa1ea5f094293efe056502d4fdc16e294dc1
서예빈 <115405820+CUTEJAY0225@users.noreply.github.com>
2022-10-10 00:08:32 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 108 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        int b = 543;
+        System.out.println(y-b);
+    }
+}
